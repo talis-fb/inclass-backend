21b1df9084f89c8dc30d9b37d1e9f7e9f30e67cf
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-03-15 13:25:30 -0300

    scripts basicos

---
+++ fastapi/more_robust.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+
+BANCO_DE_DADOS = [
+    {"id": 1, "name": "João"},
+    {"id": 2, "name": "Maria"},
+    {"id": 3, "name": "Pedro"},
+]
+
+app = FastAPI()
+
+@app.get("/user/{id}")
+def get_user(id: int):
+    for user in BANCO_DE_DADOS:
+        if user["id"] == id:
+            return user
+
+@app.post("/user/{name}")
+def create_user(name: str):
+    new_id = len(BANCO_DE_DADOS) + 1
+    new_user = {
+        "id": new_id,
+        "name": name
+    }
+
+    BANCO_DE_DADOS.append(new_user)
+
+    return {
+        "id": new_id
+    }
+
+@app.put("/user/{id}")
+def update_user(id: int, item: str):
+    for user in BANCO_DE_DADOS:
+        if user["id"] == id:
+            user["name"] = item
+            return user
